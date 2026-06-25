bd8838d5058635ba36dd3a8651a02e47362b7e4b
Leo Wolff <thewolfiness@gmail.com>
2026-06-25 22:53:32 +0200

    styles set up & hero section

--- app/pages/website/index.vue
+++ app/pages/website/index.vue
@@ -1,3 +1,16 @@
 <template>
-	<h1>Website</h1>
+	<main>
+		<section id="hero">
+			<div>
+				<img src="@img/logo.svg" />
+				<div>
+					<div>
+						<h1>Story Bean</h1>
+						<p>A virtual café to come together and write your stories.</p>
+					</div>
+					<div><button>Sign up</button><button>Log in</button></div>
+				</div>
+			</div>
+		</section>
+	</main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
+	alias: {
+		'@': import.meta.dirname + '/',
+		'@img': import.meta.dirname + '/app/assets/img',
+	},
+	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
+	css: ['./app/assets/styles/global.scss'],
 })
